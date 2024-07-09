a97c13687203e496d7cf88e11cb1d18c0de4ccb9
Paloma Misiac <paloma.misiac@gmail.com>
2024-07-09 01:47:28 -0300

    Revert "vevn+cambios+modularizacion"
    This reverts commit 30f3cfb364660b6e610f2ebb9e20c96f71015543.

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
-    MYSQL_DB = 'zero_gluten_db'
